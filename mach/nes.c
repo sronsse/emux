dcc0773b3ae7ae6196bc3ca9dd2276fc69f23f67
Sebastien Ronsse <sronsse@gmail.com>
2013-03-04 23:12:24 -0800

    mach: added NES work ram region.

--- mach/nes.c
+++ mach/nes.c
@@ -3,11 +3,75 @@
 #include <stdlib.h>
 #include <machine.h>
 #include <memory.h>
+#include <resource.h>
 #include <controllers/mapper/nes_mapper.h>
 
+#define WORK_RAM_START		0x0000
+#define WORK_RAM_END		0x07FF
+#define WORK_RAM_MIRROR_START	0x0800
+#define WORK_RAM_MIRROR_END	0x1FFF
+
 static void nes_print_usage();
+static uint8_t wram_readb(struct region *region, uint16_t address);
+static uint16_t wram_readw(struct region *region, uint16_t address);
+static void wram_writeb(struct region *region, uint8_t b, uint16_t address);
+static void wram_writew(struct region *region, uint16_t w, uint16_t address);
+
+static struct resource wram_area = {
+	.name = "wram",
+	.start = WORK_RAM_START,
+	.end = WORK_RAM_END,
+	.type = RESOURCE_MEM
+};
+
+static struct resource wram_mirror = {
+	.name = "wram_mirror",
+	.start = WORK_RAM_MIRROR_START,
+	.end = WORK_RAM_MIRROR_END,
+	.type = RESOURCE_MEM
+};
+
+static struct mops wram_mops = {
+	.readb = wram_readb,
+	.readw = wram_readw,
+	.writeb = wram_writeb,
+	.writew = wram_writew
+};
+
+static struct region wram_region = {
+	.area = &wram_area,
+	.mirrors = &wram_mirror,
+	.num_mirrors = 1,
+	.mops = &wram_mops
+};
 
 static struct nes_mapper_mach_data nes_mapper_mach_data;
+
+
+uint8_t wram_readb(struct region *region, uint16_t address)
+{
+	uint8_t *mem = (uint8_t *)region->data + address;
+	return *mem;
+}
+
+uint16_t wram_readw(struct region *region, uint16_t address)
+{
+	uint8_t *mem = (uint8_t *)region->data + address;
+	return (*(mem + 1) << 8) | *mem;
+}
+
+void wram_writeb(struct region *region, uint8_t b, uint16_t address)
+{
+	uint8_t *mem = (uint8_t *)region->data + address;
+	*mem = b;
+}
+
+void wram_writew(struct region *region, uint16_t w, uint16_t address)
+{
+	uint8_t *mem = (uint8_t *)region->data + address;
+	*mem++ = w;
+	*mem = w >> 8;
+}
 
 void nes_print_usage()
 {
@@ -23,6 +87,10 @@ bool nes_init()
 		return false;
 	}
 
+	/* Add work ram region */
+	wram_region.data = malloc(WORK_RAM_END - WORK_RAM_START + 1);
+	memory_region_add(&wram_region);
+
 	/* Add CPU and controllers */
 	cpu_add("rp2a03");
 	controller_add("ppu", NULL);
@@ -33,6 +101,7 @@ bool nes_init()
 
 void nes_deinit()
 {
+	free(wram_region.data);
 }
 
 MACHINE_START(nes, "Nintendo Entertainment System")
