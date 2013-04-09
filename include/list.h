6d97a2c0c897bc5fd7533eabc45ee70aeaa4fcf6
Sebastien Ronsse <sronsse@gmail.com>
2013-04-08 23:45:42 -0700

    main: added linked list helper functions.

---
+++ include/list.h
@@ -0,0 +1,15 @@
+#ifndef _LIST_H
+#define _LIST_H
+
+struct list_link {
+	void *data;
+	struct list_link *next;
+};
+
+void list_insert(struct list_link **list, void *data);
+void *list_get_next(struct list_link **link);
+void list_remove(struct list_link **list, void *data);
+void list_remove_all(struct list_link **list);
+
+#endif
+
