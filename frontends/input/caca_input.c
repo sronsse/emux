3557ca26125f20fa5bc2b6bcdf5c46285c6c876d
Sebastien Ronsse <sronsse@gmail.com>
2013-04-20 15:44:38 -0700

    frontends: libcaca input now reports key events.

--- frontends/input/caca_input.c
+++ frontends/input/caca_input.c
@@ -19,11 +19,19 @@ void caca_update()
 {
 	caca_event_t e;
 	struct input_event event;
+	struct input_state state;
 
 	/* Poll all events out of queue */
 	while (caca_get_event(dp, CACA_EVENT_ANY, &e, 0)) {
 		/* Fill input event structure according to detected event */
 		switch (e.type) {
+		case CACA_EVENT_KEY_PRESS:
+		case CACA_EVENT_KEY_RELEASE:
+			event.type = EVENT_KEYBOARD;
+			event.keyboard.key = caca_get_event_key_ch(&e);
+			state.active = (e.type == CACA_EVENT_KEY_PRESS);
+			input_report(&event, &state);
+			break;
 		case CACA_EVENT_QUIT:
 			event.type = EVENT_QUIT;
 			input_report(&event, NULL);
