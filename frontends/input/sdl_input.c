0975f3ecc2a47bb6b40edaae2cd01bf1230c7a60
Sebastien Ronsse <sronsse@gmail.com>
2013-04-20 15:25:21 -0700

    frontends: SDL input now reports key events.

--- frontends/input/sdl_input.c
+++ frontends/input/sdl_input.c
@@ -16,11 +16,19 @@ void sdl_update()
 {
 	SDL_Event e;
 	struct input_event event;
+	struct input_state state;
 
 	/* Poll all events out of queue */
 	while (SDL_PollEvent(&e)) {
 		/* Fill input event structure according to detected event */
 		switch (e.type) {
+		case SDL_KEYDOWN:
+		case SDL_KEYUP:
+			event.type = EVENT_KEYBOARD;
+			event.keyboard.key = e.key.keysym.sym;
+			state.active = (e.key.state == SDL_PRESSED);
+			input_report(&event, &state);
+			break;
 		case SDL_QUIT:
 			event.type = EVENT_QUIT;
 			input_report(&event, NULL);
