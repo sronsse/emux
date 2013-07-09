a60c845b4878a85b02dc0eaab2b60ddb74b72934
Sebastien Ronsse <sronsse@gmail.com>
2013-07-08 21:20:11 -0700

    controllers: implemented NES sprite RAM in PPU and reorganized read/write operations.

--- controllers/video/ppu.c
+++ controllers/video/ppu.c
@@ -9,7 +9,7 @@
 #include <util.h>
 #include <video.h>
 
-#define N_REGS				8
+#define NUM_REGS			8
 #define CONTROL_REG_1_ADDR		0
 #define CONTROL_REG_2_ADDR		1
 #define STATUS_REG_ADDR			2
@@ -18,6 +18,8 @@
 #define BG_SCROLLING_OFFSET_ADDR	5
 #define VRAM_ADDR_REG_ADDR		6
 #define VRAM_READ_WRITE_DATA_REG_ADDR	7
+
+#define SPR_RAM_SIZE			255
 
 #define SCREEN_WIDTH			256
 #define SCREEN_HEIGHT			240
@@ -60,7 +62,7 @@ struct ppu_status_reg {
 
 struct ppu {
 	union {
-		uint8_t regs[N_REGS];
+		uint8_t regs[NUM_REGS];
 		struct {
 			struct ppu_control_reg_1 control_reg_1;
 			struct ppu_control_reg_2 control_reg_2;
@@ -72,6 +74,7 @@ struct ppu {
 			uint8_t vram_read_write_data_reg;
 		};
 	};
+	uint8_t spr_ram[SPR_RAM_SIZE];
 	int current_dot;
 	int current_scanline;
 	bool first_frame;
@@ -94,59 +97,80 @@ static struct mops ppu_mops = {
 uint8_t ppu_readb(region_data_t *data, uint16_t address)
 {
 	struct ppu *ppu = data;
-	uint8_t b = ppu->regs[address];
+	uint8_t b;
 
-	/* During first frame after reset, port 2007 is read-protected */
-	if (ppu->first_frame && (address == VRAM_READ_WRITE_DATA_REG_ADDR))
-		return 0;
-
-	/* Control register 1, control register 2, spr-ram address register,
-	spr-ram data register, background scrolling offset, and vram address
-	register are write-only */
-	if ((address == CONTROL_REG_1_ADDR) ||
-		(address == CONTROL_REG_2_ADDR) ||
-		(address == SPR_RAM_ADDR_REG_ADDR) ||
-		(address == SPR_RAM_DATA_REG_ADDR) ||
-		(address == BG_SCROLLING_OFFSET_ADDR) ||
-		(address == VRAM_ADDR_REG_ADDR))
-		return 0;
-
-	/* Status register reads reset ports 2005/2006 first write flipflop */
-	if (address == STATUS_REG_ADDR) {
+	switch (address) {
+	case STATUS_REG_ADDR:
+		/* SR reads reset ports 2005/2006 first write flipflop */
 		ppu->flipflop_first_write = true;
 
-		/* Reading status register during VBLANK clears the flag */
+		/* Reading status register during VBLANK clears the flag but we
+		should still return its old state */
+		b = ppu->regs[STATUS_REG_ADDR];
 		if (ppu->status_reg.vblank_flag)
 			ppu->status_reg.vblank_flag = 0;
-	} else if (address == VRAM_READ_WRITE_DATA_REG_ADDR) {
+		return b;
+	case SPR_RAM_DATA_REG_ADDR:
+		/* Read from sprite RAM */
+		return ppu->spr_ram[ppu->regs[SPR_RAM_ADDR_REG_ADDR]];
+	case VRAM_READ_WRITE_DATA_REG_ADDR:
+		/* During first frame after reset, port is read-protected */
+		if (ppu->first_frame)
+			return 0;
+
 		/* Read from VRAM increasing address accordingly */
 		b = memory_readb(ppu->bus_id, ppu->vram_address);
 		ppu->vram_address +=
 			ppu->control_reg_1.port_2007_vram_addr_increment ?
 			32 : 1;
+		return b;
+	case CONTROL_REG_1_ADDR:
+	case CONTROL_REG_2_ADDR:
+	case SPR_RAM_ADDR_REG_ADDR:
+	case BG_SCROLLING_OFFSET_ADDR:
+	case VRAM_ADDR_REG_ADDR:
+	default:
+		/* Ports are write-only */
+		return 0;
 	}
-
-	return b;
 }
 
 void ppu_writeb(region_data_t *data, uint8_t b, uint16_t address)
 {
 	struct ppu *ppu = data;
 
-	/* Status register is write-protected */
-	if (address == STATUS_REG_ADDR)
+	switch (address) {
+	case CONTROL_REG_1_ADDR:
+		/* During first frame after reset, port is write-protected */
+		if (!ppu->first_frame)
+			ppu->regs[CONTROL_REG_1_ADDR] = b;
 		return;
-
-	/* During first frame after reset, ports 2000, 2001, 2005, and 2006
-	are write-protected */
-	if (ppu->first_frame && ((address == CONTROL_REG_1_ADDR) ||
-		(address == CONTROL_REG_2_ADDR) ||
-		(address == BG_SCROLLING_OFFSET_ADDR) ||
-		(address == VRAM_ADDR_REG_ADDR)))
+	case CONTROL_REG_2_ADDR:
+		/* During first frame after reset, port is write-protected */
+		if (!ppu->first_frame)
+			ppu->regs[CONTROL_REG_2_ADDR] = b;
 		return;
+	case SPR_RAM_ADDR_REG_ADDR:
+		/* Specifies sprite RAM address for later access */
+		ppu->regs[SPR_RAM_ADDR_REG_ADDR] = b;
+		return;
+	case SPR_RAM_DATA_REG_ADDR:
+		/* Store byte to sprite RAM */
+		ppu->spr_ram[ppu->regs[SPR_RAM_ADDR_REG_ADDR]] = b;
 
-	if ((address == BG_SCROLLING_OFFSET_ADDR) ||
-		(address == VRAM_ADDR_REG_ADDR)) {
+		/* Sprite RAM address is incremented after every data write */
+		ppu->regs[SPR_RAM_ADDR_REG_ADDR]++;
+		return;
+	case BG_SCROLLING_OFFSET_ADDR:
+		/* During first frame after reset, port is write-protected */
+		if (!ppu->first_frame)
+			ppu->regs[BG_SCROLLING_OFFSET_ADDR] = b;
+		return;
+	case VRAM_ADDR_REG_ADDR:
+		/* During first frame after reset, port is write-protected */
+		if (ppu->first_frame)
+			return;
+
 		/* Handle 1st write */
 		if (ppu->flipflop_first_write) {
 			ppu->flipflop_first_write = false;
@@ -164,16 +188,18 @@ void ppu_writeb(region_data_t *data, uint8_t b, uint16_t address)
 		}
 		ppu->flipflop_first_write = true;
 		return;
-	} else if (address == VRAM_READ_WRITE_DATA_REG_ADDR) {
+	case VRAM_READ_WRITE_DATA_REG_ADDR:
 		/* Write to stored VRAM address and increment accordingly */
 		memory_writeb(ppu->bus_id, b, ppu->vram_address);
 		ppu->vram_address +=
 			ppu->control_reg_1.port_2007_vram_addr_increment ?
 			32 : 1;
 		return;
+	case STATUS_REG_ADDR:
+	default:
+		/* Ports are write-protected */
+		return;
 	}
-
-	ppu->regs[address] = b;
 }
 
 bool ppu_init(struct controller_instance *instance)
