0f1b3183fe9e9e5dbdd6e3f7a4b7ca19b77f95f7
Sebastien Ronsse <sronsse@gmail.com>
2015-08-19 12:47:29 -0700

    controllers: updated PAPU channel definitions and read/write routines.

--- controllers/audio/papu.c
+++ controllers/audio/papu.c
@@ -5,32 +5,43 @@
 #include <bitops.h>
 #include <clock.h>
 #include <controller.h>
-#include <log.h>
 #include <memory.h>
-#include <util.h>
 
-#define NUM_REGS	23
-#define NR10		0x00
-#define NR11		0x01
-#define NR12		0x02
-#define NR13		0x03
-#define NR14		0x04
-#define NR21		0x06
-#define NR22		0x07
-#define NR23		0x08
-#define NR24		0x09
-#define NR30		0x0A
-#define NR31		0x0B
-#define NR32		0x0C
-#define NR33		0x0D
-#define NR34		0x0E
-#define NR41		0x10
-#define NR42		0x11
-#define NR43		0x12
-#define NR44		0x13
-#define NR50		0x14
-#define NR51		0x15
-#define NR52		0x16
+#define NUM_REGS		23
+#define NR10			0x00
+#define NR11			0x01
+#define NR12			0x02
+#define NR13			0x03
+#define NR14			0x04
+#define NR21			0x06
+#define NR22			0x07
+#define NR23			0x08
+#define NR24			0x09
+#define NR30			0x0A
+#define NR31			0x0B
+#define NR32			0x0C
+#define NR33			0x0D
+#define NR34			0x0E
+#define NR41			0x10
+#define NR42			0x11
+#define NR43			0x12
+#define NR44			0x13
+#define NR50			0x14
+#define NR51			0x15
+#define NR52			0x16
+
+#define NUM_CHANNELS		4
+#define NUM_SQUARE_STEPS	8
+#define NUM_FRAME_SEQ_STEPS	8
+#define MAX_SQUARE_LEN_COUNTER	64
+#define MAX_WAVE_LEN_COUNTER	256
+#define MAX_NOISE_LEN_COUNTER	64
+#define MAX_FREQ		2048
+#define MAX_VOLUME		0x0F
+#define FRAME_SEQ_RATE		512
+#define SQUARE_FREQ_MUL		4
+#define WAVE_RAM_SIZE		16
+#define WAVE_FREQ_MUL		2
 
 union channel_sweep {
 	uint8_t raw;
@@ -90,8 +101,8 @@ union channel_poly {
 	uint8_t raw;
 	struct {
 		uint8_t ratio:3;
-		uint8_t step:1;
-		uint8_t freq:4;
+		uint8_t width:1;
+		uint8_t shift:4;
 	};
 };
 
@@ -160,19 +171,54 @@ union papu_regs {
 	};
 };
 
-struct channel {
+struct channel1 {
 	uint8_t value;
 	uint8_t step;
 	uint8_t volume;
 	uint16_t counter;
-	uint16_t len_counter;
+	uint8_t len_counter;
+	uint8_t env_counter;
+	uint16_t sweep_freq;
+	uint16_t sweep_counter;
+	uint16_t lfsr;
 	bool enabled;
+	bool sweep_enabled;
+};
+
+struct channel2 {
+	bool enabled;
+	uint8_t value;
+	uint8_t step;
+	uint8_t volume;
+	uint16_t counter;
+	uint8_t len_counter;
+	uint8_t env_counter;
+};
+
+struct channel3 {
+	bool enabled;
+	uint8_t pos;
+	uint8_t sample;
+	uint16_t counter;
+	uint16_t len_counter;
+};
+
+struct channel4 {
+	bool enabled;
+	uint8_t value;
+	uint8_t volume;
+	uint16_t counter;
+	uint8_t len_counter;
+	uint8_t env_counter;
+	uint16_t lfsr;
 };
 
 struct papu {
 	union papu_regs regs;
-	struct channel channel1;
-	struct channel channel2;
+	struct channel1 channel1;
+	struct channel2 channel2;
+	struct channel3 channel3;
+	struct channel4 channel4;
 	uint8_t wave_ram[WAVE_RAM_SIZE];
 	struct region region;
 	struct region wave_region;
@@ -184,8 +230,10 @@ static void papu_reset(struct controller_instance *instance);
 static void papu_deinit(struct controller_instance *instance);
 static uint8_t papu_readb(struct papu *papu, address_t address);
 static void papu_writeb(struct papu *papu, uint8_t b, address_t address);
-static void channel1_write(struct papu *papu, uint8_t b, address_t address);
-static void channel2_write(struct papu *papu, uint8_t b, address_t address);
+static void channel1_write(struct papu *papu, address_t address);
+static void channel2_write(struct papu *papu, address_t address);
+static void channel3_write(struct papu *papu, address_t address);
+static void channel4_write(struct papu *papu, address_t address);
 static void papu_tick(struct papu *papu);
 static void square_update(struct channel *channel, uint16_t freq, uint8_t duty);
 
@@ -197,67 +245,38 @@ static struct mops papu_mops = {
 uint8_t papu_readb(struct papu *papu, address_t address)
 {
 	uint8_t b;
-	union channel_sweep chan_sweep;
-	union channel_len chan_len;
-	union channel_freq chan_freq;
-	union sound_ctrl sound_ctrl;
-	union channel_on_off chan_on_off;
-	union channel_lvl chan_lvl;
 
-	/* Read register and process it (some fields are read back as 1) */
+	/* Read requested register */
+	b = papu->regs.raw[address];
+
+	/* Handle each register read (OR'ing it with specific values) */
 	switch (address) {
 	case NR10:
-		chan_sweep.raw = papu->regs.raw[address];
-		chan_sweep.reserved = 1;
-		b = chan_sweep.raw;
-		break;
+		return b | 0x80;
 	case NR11:
 	case NR21:
-		chan_len.raw = papu->regs.raw[address];
-		chan_len.data = 0x3F;
-		b = chan_len.raw;
-		break;
-	case NR13:
-	case NR23:
-	case NR31:
-	case NR33:
-	case NR41:
-		b = 0xFF;
-		break;
+		return b | 0x3F;
+	case NR12:
+	case NR22:
+	case NR42:
+	case NR43:
+	case NR50:
+	case NR51:
+		return b;
 	case NR14:
 	case NR24:
 	case NR34:
 	case NR44:
-		chan_freq.raw = papu->regs.raw[address];
-		chan_freq.freq_high = 0x07;
-		chan_freq.reserved = 0x07;
-		chan_freq.initial = 1;
-		b = chan_freq.raw;
-		break;
+		return b | 0xBF;
 	case NR30:
-		chan_on_off.raw = papu->regs.raw[address];
-		b = chan_on_off.raw;
-		chan_on_off.reserved = 0x7F;
-		b = chan_on_off.raw;
-		break;
+		return b | 0x7F;
 	case NR32:
-		chan_lvl.raw = papu->regs.raw[address];
-		chan_lvl.reserved1 = 0x1F;
-		chan_lvl.reserved2 = 1;
-		b = chan_lvl.raw;
-		break;
+		return b | 0x9F;
 	case NR52:
-		sound_ctrl.raw = papu->regs.raw[address];
-		sound_ctrl.reserved = 0x07;
-		b = sound_ctrl.raw;
-		break;
+		return b | 0x70;
 	default:
-		b = papu->regs.raw[address];
-		break;
+		return 0xFF;
 	}
-
-	/* Return processed register value */
-	return b;
 }
 
 void papu_writeb(struct papu *papu, uint8_t b, address_t address)
@@ -281,7 +300,6 @@ void papu_writeb(struct papu *papu, uint8_t b, address_t address)
 		return;
 
 	/* Write to requested register */
-	/* TODO: move this */
 	papu->regs.raw[address] = b;
 
 	/* Handle writes to each channel separately */
@@ -291,27 +309,46 @@ void papu_writeb(struct papu *papu, uint8_t b, address_t address)
 	case NR12:
 	case NR13:
 	case NR14:
-		channel1_write(papu, b, address);
+		channel1_write(papu, address);
 		break;
 	case NR21:
 	case NR22:
 	case NR23:
 	case NR24:
-		channel2_write(papu, b, address);
+		channel2_write(papu, address);
+		break;
+	case NR30:
+	case NR31:
+	case NR32:
+	case NR33:
+	case NR34:
+		channel3_write(papu, address);
+		break;
+	case NR41:
+	case NR42:
+	case NR43:
+	case NR44:
+		channel4_write(papu, address);
 		break;
 	default:
 		break;
 	}
 }
 
-void channel1_write(struct papu *papu, uint8_t UNUSED(b), address_t address)
+void channel1_write(struct papu *papu, address_t address)
 {
+	uint8_t v;
+	uint16_t freq;
+	uint16_t counter;
+	uint16_t d;
 	bool dac_on;
+	bool dec;
 
 	switch (address) {
 	case NR11:
 		/* Reload length counter */
-		papu->channel1.len_counter = papu->regs.nr11.data;
+		v = MAX_SQUARE_LEN_COUNTER - papu->regs.nr11.data;
+		papu->channel1.len_counter = v;
 		break;
 	case NR12:
 		/* Handle DAC power off */
@@ -320,21 +357,58 @@ void channel1_write(struct papu *papu, uint8_t UNUSED(b), address_t address)
 			papu->channel1.enabled = false;
 		break;
 	case NR14:
-		/* Handle trigger event */
-		if (papu->regs.nr14.initial) {
-			/* Enable channel if DAC is on (controlled by NR12) */
-			dac_on = bitops_getb(&papu->regs.nr12.raw, 3, 5);
-			papu->channel1.enabled = dac_on;
+		/* Break if trigger event is not required */
+		if (!papu->regs.nr14.initial)
+			break;
 
-			/* Reload length counter if needed */
-			if (papu->channel1.len_counter == 0)
-				papu->channel1.len_counter = 64;
+		/* Enable channel if DAC is on (controlled by NR12) */
+		dac_on = bitops_getb(&papu->regs.nr12.raw, 3, 5);
+		papu->channel1.enabled = dac_on;
 
-			/* TODO:
-			Frequency timer is reloaded with period.
-			Volume envelope timer is reloaded with period.
-			Channel volume is reloaded from NRx2.
-			Square 1's sweep does several things. */
+		/* Reload length counter if needed */
+		if (papu->channel1.len_counter == 0)
+			papu->channel1.len_counter = MAX_SQUARE_LEN_COUNTER;
+
+		/* Reload frequency timer with period */
+		freq = papu->regs.nr13 | (papu->regs.nr14.freq_high << 8);
+		papu->channel1.counter = (MAX_FREQ - freq) * SQUARE_FREQ_MUL;
+
+		/* Reload volume envelope timer with period */
+		papu->channel1.env_counter = papu->regs.nr12.num_sweep;
+
+		/* Reload channel volume */
+		papu->channel1.volume = papu->regs.nr12.initial_volume;
+
+		/* Copy frequency shadow register and reload sweep timer */
+		papu->channel1.sweep_freq = freq;
+		papu->channel1.sweep_counter = papu->regs.nr10.time;
+
+		/* Set/reset internal enabled flag based on period and shift */
+		papu->channel1.sweep_enabled = (papu->regs.nr10.time != 0) ||
+			(papu->regs.nr10.num_shift != 0);
+
+		/* Perform frequency calculation and overflow check if needed */
+		if (papu->regs.nr10.time != 0) {
+			/* Update sweep shadow frequency */
+			v = papu->regs.nr10.num_shift;
+			dec = papu->regs.nr10.inc_dec;
+			d = papu->channel1.sweep_freq >> v;
+			papu->channel1.sweep_freq += dec ? -d : d;
+
+			/* Write frequency back to channel registers */
+			freq = papu->channel1.sweep_freq;
+			papu->regs.nr13 = freq & 0xFF;
+			papu->regs.nr14.freq_high = freq >> 8;
+
+			/* Reload frequency timer */
+			counter = (MAX_FREQ - freq) * SQUARE_FREQ_MUL;
+			papu->channel1.counter = counter;
+
+			/* Disable channel if maximum frequency is reached */
+			if (papu->channel1.sweep_freq >= MAX_FREQ) {
+				papu->channel1.enabled = false;
+				papu->channel1.sweep_enabled = false;
+			}
 		}
 		break;
 	default:
@@ -342,14 +416,17 @@ void channel1_write(struct papu *papu, uint8_t UNUSED(b), address_t address)
 	}
 }
 
-void channel2_write(struct papu *papu, uint8_t UNUSED(b), address_t address)
+void channel2_write(struct papu *papu, address_t address)
 {
+	uint8_t v;
+	uint16_t freq;
 	bool dac_on;
 
 	switch (address) {
 	case NR21:
 		/* Reload length counter */
-		papu->channel1.len_counter = papu->regs.nr11.data;
+		v = MAX_SQUARE_LEN_COUNTER - papu->regs.nr21.data;
+		papu->channel2.len_counter = v;
 		break;
 	case NR22:
 		/* Handle DAC power off */
@@ -358,21 +435,113 @@ void channel2_write(struct papu *papu, uint8_t UNUSED(b), address_t address)
 			papu->channel2.enabled = false;
 		break;
 	case NR24:
-		/* Handle trigger event */
-		if (papu->regs.nr24.initial) {
-			/* Enable channel if DAC is on (controlled by NR22) */
-			dac_on = bitops_getb(&papu->regs.nr22.raw, 3, 5);
-			papu->channel2.enabled = dac_on;
+		/* Break if trigger event is not required */
+		if (!papu->regs.nr24.initial)
+			break;
 
-			/* Reload length counter if needed */
-			if (papu->channel2.len_counter == 0)
-				papu->channel2.len_counter = 64;
+		/* Enable channel if DAC is on (controlled by NR22) */
+		dac_on = bitops_getb(&papu->regs.nr22.raw, 3, 5);
+		papu->channel2.enabled = dac_on;
 
-			/* TODO:
-			Frequency timer is reloaded with period.
-			Volume envelope timer is reloaded with period.
-			Channel volume is reloaded from NRx2. */
-		}
+		/* Reload length counter if needed */
+		if (papu->channel2.len_counter == 0)
+			papu->channel2.len_counter = MAX_SQUARE_LEN_COUNTER;
+
+		/* Reload frequency timer with period */
+		freq = papu->regs.nr23 | (papu->regs.nr24.freq_high << 8);
+		papu->channel2.counter = (MAX_FREQ - freq) * SQUARE_FREQ_MUL;
+
+		/* Reload volume envelope timer with period */
+		papu->channel2.env_counter = papu->regs.nr22.num_sweep;
+
+		/* Reload channel volume */
+		papu->channel2.volume = papu->regs.nr22.initial_volume;
+		break;
+	default:
+		break;
+	}
+}
+
+void channel3_write(struct papu *papu, address_t address)
+{
+	uint8_t v;
+	uint16_t freq;
+
+	switch (address) {
+	case NR30:
+		/* Handle DAC power off */
+		if (!papu->regs.nr30.on)
+			papu->channel3.enabled = false;
+		break;
+	case NR31:
+		/* Reload length counter */
+		v = MAX_WAVE_LEN_COUNTER - papu->regs.nr31;
+		papu->channel3.len_counter = v;
+		break;
+	case NR34:
+		/* Break if trigger event is not required */
+		if (!papu->regs.nr34.initial)
+			break;
+
+		/* Enable channel if DAC is on (controlled by NR30) */
+		papu->channel3.enabled = papu->regs.nr30.on;
+
+		/* Reload length counter if needed */
+		if (papu->channel3.len_counter == 0)
+			papu->channel3.len_counter = MAX_WAVE_LEN_COUNTER;
+
+		/* Reload frequency timer with period */
+		freq = papu->regs.nr33 | (papu->regs.nr34.freq_high << 8);
+		papu->channel3.counter = (MAX_FREQ - freq) * WAVE_FREQ_MUL;
+		break;
+	default:
+		break;
+	}
+}
+
+void channel4_write(struct papu *papu, address_t address)
+{
+	uint8_t v;
+	uint16_t counter;
+	bool dac_on;
+
+	switch (address) {
+	case NR41:
+		/* Reload length counter */
+		v = MAX_NOISE_LEN_COUNTER - papu->regs.nr41.data;
+		papu->channel4.len_counter = v;
+		break;
+	case NR42:
+		/* Handle DAC power off */
+		dac_on = bitops_getb(&papu->regs.nr42.raw, 3, 5);
+		if (!dac_on)
+			papu->channel4.enabled = false;
+		break;
+	case NR44:
+		/* Break if trigger event is not required */
+		if (!papu->regs.nr44.initial)
+			break;
+
+		/* Enable channel if DAC is on (controlled by NR42) */
+		dac_on = bitops_getb(&papu->regs.nr42.raw, 3, 5);
+		papu->channel4.enabled = dac_on;
+
+		/* Reload length counter if needed */
+		if (papu->channel4.len_counter == 0)
+			papu->channel4.len_counter = MAX_NOISE_LEN_COUNTER;
+
+		/* Reload frequency timer with period */
+		counter = noise_get_divisor(papu) << papu->regs.nr43.shift;
+		papu->channel4.counter = counter;
+
+		/* Reload volume envelope timer with period */
+		papu->channel4.env_counter = papu->regs.nr42.num_sweep;
+
+		/* Reload channel volume */
+		papu->channel4.volume = papu->regs.nr42.initial_volume;
+
+		/* Reset LFSR */
+		papu->channel4.lfsr = 0x7FFF;
 		break;
 	default:
 		break;
@@ -508,10 +677,16 @@ void papu_reset(struct controller_instance *instance)
 {
 	struct papu *papu = instance->priv_data;
 
-	/* Initialize registers and channels */
+	/* Initialize controller data */
 	memset(papu->regs.raw, 0, NUM_REGS * sizeof(uint8_t));
-	papu->channel1.enabled = false;
-	papu->channel2.enabled = false;
+	memset(&papu->channel1, 0, sizeof(struct channel1));
+	memset(&papu->channel2, 0, sizeof(struct channel2));
+	memset(&papu->channel3, 0, sizeof(struct channel3));
+	memset(&papu->channel4, 0, sizeof(struct channel4));
+	papu->seq_step = 0;
+
+	/* Initialize noise channel linear feedback shift register */
+	papu->channel4.lfsr = 0x7FFF;
 }
 
 void papu_deinit(struct controller_instance *instance)
