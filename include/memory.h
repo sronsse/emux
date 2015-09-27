c9d54e7e693f64be8fd0c263e672c950459c66c3
Sebastien Ronsse <sronsse@gmail.com>
2015-09-26 18:51:24 -0700

    main: inlined memory read/write methods to improve performance.

--- include/memory.h
+++ include/memory.h
@@ -4,25 +4,16 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <list.h>
+#include <log.h>
 #include <resource.h>
 
 #define KB(x) (x * 1024)
 #define MB(x) (x * 1024 * 1024)
 
-#define DECLARE_MEMORY_READ(ext, type) \
-	type memory_read##ext(int bus_id, address_t address);
-#define DECLARE_MEMORY_WRITE(ext, type) \
-	void memory_write##ext(int bus_id, type data, address_t address);
-
 #define DECLARE_MEMORY_READ_OP(ext, type) \
 	typedef type (*read##ext##_t)(region_data_t *, address_t);
 #define DECLARE_MEMORY_WRITE_OP(ext, type) \
 	typedef void (*write##ext##_t)(region_data_t *, type, address_t);
-
-#define DECLARE_DMA_READ(ext, type) \
-	type dma_read##ext(int channel);
-#define DECLARE_DMA_WRITE(ext, type) \
-	void dma_write##ext(int channel, type data);
 
 #define DECLARE_DMA_READ_OP(ext, type) \
 	typedef type (*dma_read##ext##_t)(dma_channel_data_t *);
@@ -93,28 +84,210 @@ void memory_bus_remove_all();
 bool memory_region_add(struct region *region);
 void memory_region_remove(struct region *region);
 
-/* Declare memory read/write functions */
-DECLARE_MEMORY_READ(b, uint8_t)
-DECLARE_MEMORY_READ(w, uint16_t)
-DECLARE_MEMORY_READ(l, uint32_t)
-DECLARE_MEMORY_WRITE(b, uint8_t)
-DECLARE_MEMORY_WRITE(w, uint16_t)
-DECLARE_MEMORY_WRITE(l, uint32_t)
-
 void dma_channel_add(struct dma_channel *channel);
 void dma_channel_remove(struct dma_channel *channel);
 void dma_channel_remove_all();
 
-/* Declare DMA read/write functions */
-DECLARE_DMA_READ(b, uint8_t)
-DECLARE_DMA_READ(w, uint16_t)
-DECLARE_DMA_READ(l, uint32_t)
-DECLARE_DMA_WRITE(b, uint8_t)
-DECLARE_DMA_WRITE(w, uint16_t)
-DECLARE_DMA_WRITE(l, uint32_t)
-
+extern struct list_link *busses;
+extern struct list_link *dma_channels;
 extern struct mops rom_mops;
 extern struct mops ram_mops;
+
+#define DEFINE_MEMORY_READ(ext, type) \
+	static inline type memory_read##ext(int bus_id, address_t address) \
+	{ \
+		struct bus *bus; \
+		struct region *r; \
+		struct resource *mirror; \
+		struct list_link *link; \
+		address_t a; \
+		int i; \
+	\
+		/* Find bus with matching ID */ \
+		link = busses; \
+		while ((bus = list_get_next(&link)) && (bus->id != bus_id)); \
+	\
+		/* Return 0 if none was found */ \
+		if (!bus) { \
+			LOG_W("Bus not found (%s(%u, %08x))!\n", \
+				__func__, \
+				bus_id, \
+				address); \
+			return 0; \
+		} \
+	\
+		/* Parse regions */ \
+		link = bus->regions; \
+		while ((r = list_get_next(&link))) { \
+			/* Skip if region if operation is not supported */ \
+			if (!r->mops->read##ext) \
+				continue; \
+	\
+			/* Call operation if address is within area */ \
+			if ((address >= r->area->data.mem.start) && \
+				(address <= r->area->data.mem.end)) { \
+				a = address - r->area->data.mem.start; \
+				return r->mops->read##ext(r->data, a); \
+			} \
+	\
+			/* Call operation if address is within a mirror */ \
+			for (i = 0; i < r->area->num_children; i++) { \
+				mirror = &r->area->children[i]; \
+				if ((address >= mirror->data.mem.start) && \
+					(address <= mirror->data.mem.end)) { \
+					a = address - mirror->data.mem.start; \
+					return r->mops->read##ext(r->data, a); \
+				} \
+			} \
+		} \
+	\
+		/* Return 0 in case of read failure */ \
+		LOG_W("Region not found (%s(%u, %08x))!\n", \
+			__func__, \
+			bus_id, \
+			address); \
+		return 0; \
+	}
+
+#define DEFINE_MEMORY_WRITE(ext, type) \
+	static inline void memory_write##ext(int bus_id, type data, \
+		address_t address) \
+	{ \
+		struct bus *bus; \
+		struct region *r; \
+		struct resource *mirror; \
+		struct list_link *link; \
+		struct region **regions; \
+		address_t *addresses; \
+		address_t a; \
+		int num; \
+		int i; \
+	\
+		/* Find bus with matching ID */ \
+		link = busses; \
+		while ((bus = list_get_next(&link)) && (bus->id != bus_id)); \
+	\
+		/* Return on failure */ \
+		if (!bus) { \
+			LOG_W("Bus not found (%s(%u, %08x))!\n", \
+				__func__, \
+				bus_id, \
+				address); \
+			return; \
+		} \
+	\
+		/* Parse regions */ \
+		link = bus->regions; \
+		regions = NULL; \
+		addresses = NULL; \
+		num = 0; \
+		while ((r = list_get_next(&link))) { \
+			/* Skip if region if operation is not supported */ \
+			if (!r->mops->write##ext) \
+				continue; \
+	\
+			/* Add region and adapted address if needed */ \
+			if ((address >= r->area->data.mem.start) && \
+				(address <= r->area->data.mem.end)) { \
+				a = address - r->area->data.mem.start; \
+				num++; \
+				regions = realloc(regions, \
+					num * sizeof(struct region *)); \
+				addresses = realloc(addresses, \
+					num * sizeof(address_t)); \
+				regions[num - 1] = r; \
+				addresses[num - 1] = a; \
+			} \
+	\
+			/* Parse mirrors */ \
+			for (i = 0; i < r->area->num_children; i++) { \
+				mirror = &r->area->children[i]; \
+	\
+				/* Skip if address is not within mirror */ \
+				if (!((address >= mirror->data.mem.start) && \
+					(address <= mirror->data.mem.end))) \
+					continue; \
+	\
+				/* Add region and adapted address */ \
+				a = address - mirror->data.mem.start; \
+				num++; \
+				regions = realloc(regions, \
+					num * sizeof(struct region *)); \
+				addresses = realloc(addresses, \
+					num * sizeof(address_t)); \
+				regions[num - 1] = r; \
+				addresses[num - 1] = a; \
+			} \
+		} \
+	\
+		/* Call write operations */ \
+		for (i = 0; i < num; i++) \
+			regions[i]->mops->write##ext(regions[i]->data, \
+				data, \
+				addresses[i]); \
+	\
+		/* Warn on write failure */ \
+		if (num == 0) \
+			LOG_W("Region not found (%s(%u, %08x))!\n", \
+				__func__, \
+				bus_id, \
+				address); \
+	\
+		/* Free arrays */ \
+		free(regions); \
+		free(addresses); \
+	}
+
+#define DEFINE_DMA_READ(ext, type) \
+	static inline type dma_read##ext(int channel) \
+	{ \
+		struct dma_channel *ch; \
+		struct list_link *link = dma_channels; \
+	\
+		/* Find matching DMA channel and call read operation */ \
+		while ((ch = list_get_next(&link))) \
+			if ((ch->res->data.dma.channel == channel) && \
+				ch->ops->read##ext) \
+				return ch->ops->read##ext(ch->data); \
+	\
+		/* Return 0 in case of read failure */ \
+		LOG_W("DMA channel not found (%s(%u))!\n", __func__, channel); \
+		return 0; \
+	}
+
+#define DEFINE_DMA_WRITE(ext, type) \
+	static inline void dma_write##ext(int channel, type data) \
+	{ \
+		struct dma_channel *ch; \
+		struct list_link *link = dma_channels; \
+	\
+		/* Find matching DMA channel and call write operation */ \
+		while ((ch = list_get_next(&link))) \
+			if ((ch->res->data.dma.channel == channel) && \
+				ch->ops->write##ext) { \
+				ch->ops->write##ext(ch->data, data); \
+				return; \
+			} \
+	\
+		/* Warn in case of read failure */ \
+		LOG_W("DMA channel not found (%s(%u))!\n", __func__, channel); \
+	}
+
+/* Define memory read/write functions */
+DEFINE_MEMORY_READ(b, uint8_t)
+DEFINE_MEMORY_READ(w, uint16_t)
+DEFINE_MEMORY_READ(l, uint32_t)
+DEFINE_MEMORY_WRITE(b, uint8_t)
+DEFINE_MEMORY_WRITE(w, uint16_t)
+DEFINE_MEMORY_WRITE(l, uint32_t)
+
+/* Define DMA read/write functions */
+DEFINE_DMA_READ(b, uint8_t)
+DEFINE_DMA_READ(w, uint16_t)
+DEFINE_DMA_READ(l, uint32_t)
+DEFINE_DMA_WRITE(b, uint8_t)
+DEFINE_DMA_WRITE(w, uint16_t)
+DEFINE_DMA_WRITE(l, uint32_t)
 
 #endif
 
--- main/memory.c
+++ main/memory.c
@@ -3,182 +3,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <bitops.h>
-#include <log.h>
 #include <memory.h>
 #include <util.h>
-
-#define DEFINE_MEMORY_READ(ext, type) \
-	type memory_read##ext(int bus_id, address_t address) \
-	{ \
-		struct bus *bus; \
-		struct region *r; \
-		struct resource *mirror; \
-		struct list_link *link; \
-		address_t a; \
-		int i; \
-	\
-		/* Get bus and return 0 on failure */ \
-		bus = get_bus(bus_id); \
-		if (!bus) { \
-			LOG_W("Bus not found (%s(%u, %08x))!\n", \
-				__func__, \
-				bus_id, \
-				address); \
-			return 0; \
-		} \
-	\
-		/* Parse regions */ \
-		link = bus->regions; \
-		while ((r = list_get_next(&link))) { \
-			/* Skip if region if operation is not supported */ \
-			if (!r->mops->read##ext) \
-				continue; \
-	\
-			/* Call operation if address is within area */ \
-			if ((address >= r->area->data.mem.start) && \
-				(address <= r->area->data.mem.end)) { \
-				a = address - r->area->data.mem.start; \
-				return r->mops->read##ext(r->data, a); \
-			} \
-	\
-			/* Call operation if address is within a mirror */ \
-			for (i = 0; i < r->area->num_children; i++) { \
-				mirror = &r->area->children[i]; \
-				if ((address >= mirror->data.mem.start) && \
-					(address <= mirror->data.mem.end)) { \
-					a = address - mirror->data.mem.start; \
-					return r->mops->read##ext(r->data, a); \
-				} \
-			} \
-		} \
-	\
-		/* Return 0 in case of read failure */ \
-		LOG_W("Region not found (%s(%u, %08x))!\n", \
-			__func__, \
-			bus_id, \
-			address); \
-		return 0; \
-	}
-
-#define DEFINE_MEMORY_WRITE(ext, type) \
-	void memory_write##ext(int bus_id, type data, address_t address) \
-	{ \
-		struct bus *bus; \
-		struct region *r; \
-		struct resource *mirror; \
-		struct list_link *link; \
-		struct region **regions; \
-		address_t *addresses; \
-		address_t a; \
-		int num; \
-		int i; \
-	\
-		/* Get bus and return on failure */ \
-		bus = get_bus(bus_id); \
-		if (!bus) { \
-			LOG_W("Bus not found (%s(%u, %08x))!\n", \
-				__func__, \
-				bus_id, \
-				address); \
-			return; \
-		} \
-	\
-		/* Parse regions */ \
-		link = bus->regions; \
-		regions = NULL; \
-		addresses = NULL; \
-		num = 0; \
-		while ((r = list_get_next(&link))) { \
-			/* Skip if region if operation is not supported */ \
-			if (!r->mops->write##ext) \
-				continue; \
-	\
-			/* Add region and adapted address if needed */ \
-			if ((address >= r->area->data.mem.start) && \
-				(address <= r->area->data.mem.end)) { \
-				a = address - r->area->data.mem.start; \
-				num++; \
-				regions = realloc(regions, \
-					num * sizeof(struct region *)); \
-				addresses = realloc(addresses, \
-					num * sizeof(address_t)); \
-				regions[num - 1] = r; \
-				addresses[num - 1] = a; \
-			} \
-	\
-			/* Parse mirrors */ \
-			for (i = 0; i < r->area->num_children; i++) { \
-				mirror = &r->area->children[i]; \
-	\
-				/* Skip if address is not within mirror */ \
-				if (!((address >= mirror->data.mem.start) && \
-					(address <= mirror->data.mem.end))) \
-					continue; \
-	\
-				/* Add region and adapted address */ \
-				a = address - mirror->data.mem.start; \
-				num++; \
-				regions = realloc(regions, \
-					num * sizeof(struct region *)); \
-				addresses = realloc(addresses, \
-					num * sizeof(address_t)); \
-				regions[num - 1] = r; \
-				addresses[num - 1] = a; \
-			} \
-		} \
-	\
-		/* Call write operations */ \
-		for (i = 0; i < num; i++) \
-			regions[i]->mops->write##ext(regions[i]->data, \
-				data, \
-				addresses[i]); \
-	\
-		/* Warn on write failure */ \
-		if (num == 0) \
-			LOG_W("Region not found (%s(%u, %08x))!\n", \
-				__func__, \
-				bus_id, \
-				address); \
-	\
-		/* Free arrays */ \
-		free(regions); \
-		free(addresses); \
-	}
-
-#define DEFINE_DMA_READ(ext, type) \
-	type dma_read##ext(int channel) \
-	{ \
-		struct dma_channel *ch; \
-		struct list_link *link = dma_channels; \
-	\
-		/* Find matching DMA channel and call read operation */ \
-		while ((ch = list_get_next(&link))) \
-			if ((ch->res->data.dma.channel == channel) && \
-				ch->ops->read##ext) \
-				return ch->ops->read##ext(ch->data); \
-	\
-		/* Return 0 in case of read failure */ \
-		LOG_W("DMA channel not found (%s(%u))!\n", __func__, channel); \
-		return 0; \
-	}
-
-#define DEFINE_DMA_WRITE(ext, type) \
-	void dma_write##ext(int channel, type data) \
-	{ \
-		struct dma_channel *ch; \
-		struct list_link *link = dma_channels; \
-	\
-		/* Find matching DMA channel and call write operation */ \
-		while ((ch = list_get_next(&link))) \
-			if ((ch->res->data.dma.channel == channel) && \
-				ch->ops->write##ext) { \
-				ch->ops->write##ext(ch->data, data); \
-				return; \
-			} \
-	\
-		/* Warn in case of read failure */ \
-		LOG_W("DMA channel not found (%s(%u))!\n", __func__, channel); \
-	}
 
 static uint8_t rom_readb(uint8_t *rom, address_t address);
 static uint16_t rom_readw(uint8_t *rom, address_t address);
@@ -191,8 +17,8 @@ static void ram_writew(uint8_t *ram, uint16_t w, address_t address);
 static void ram_writel(uint8_t *ram, uint32_t l, address_t address);
 static struct bus *get_bus(int bus_id);
 
-static struct list_link *busses;
-static struct list_link *dma_channels;
+struct list_link *busses;
+struct list_link *dma_channels;
 
 struct mops rom_mops = {
 	.readb = (readb_t)rom_readb,
@@ -355,14 +181,6 @@ void memory_region_remove(struct region *region)
 	list_remove(&bus->regions, region);
 }
 
-/* Define memory read/write functions */
-DEFINE_MEMORY_READ(b, uint8_t)
-DEFINE_MEMORY_READ(w, uint16_t)
-DEFINE_MEMORY_READ(l, uint32_t)
-DEFINE_MEMORY_WRITE(b, uint8_t)
-DEFINE_MEMORY_WRITE(w, uint16_t)
-DEFINE_MEMORY_WRITE(l, uint32_t)
-
 void dma_channel_add(struct dma_channel *channel)
 {
 	/* Add DMA channel to list */
@@ -380,12 +198,4 @@ void dma_channel_remove_all()
 	/* Remove all DMA channels from list */
 	list_remove_all(&dma_channels);
 }
-
-/* Define DMA read/write functions */
-DEFINE_DMA_READ(b, uint8_t)
-DEFINE_DMA_READ(w, uint16_t)
-DEFINE_DMA_READ(l, uint32_t)
-DEFINE_DMA_WRITE(b, uint8_t)
-DEFINE_DMA_WRITE(w, uint16_t)
-DEFINE_DMA_WRITE(l, uint32_t)
 
