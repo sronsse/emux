534914d263a8827359126fdd7d045f46aebe9943
Sebastien Ronsse <sronsse@gmail.com>
2015-09-26 18:48:05 -0700

    main: inlined bitops methods to improve performance.

--- include/bitops.h
+++ include/bitops.h
@@ -4,25 +4,72 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-#define DECLARE_BITOPS_GET(ext, type) \
-	type bitops_get##ext(type *a, type shift, type length);
+#define DEFINE_BITOPS_GET(ext, type) \
+	static inline type bitops_get##ext(type *a, type sh, type l) \
+	{ \
+		return (*a >> sh) & (BIT(l) - 1); \
+	}
 
-#define DECLARE_BITOPS_SET(ext, type) \
-	void bitops_set##ext(type *a, type shift, type length, type v);
+#define DEFINE_BITOPS_SET(ext, type) \
+	static inline void bitops_set##ext(type *a, type sh, type l, type v) \
+	{ \
+		*a &= ~((BIT(l) - 1) << sh); \
+		*a |= (v << sh); \
+	}
 
 #define BIT(n) (1 << (n))
 
-/* Declare BITOPS functions */
-DECLARE_BITOPS_GET(b, uint8_t)
-DECLARE_BITOPS_SET(b, uint8_t)
-DECLARE_BITOPS_GET(w, uint16_t)
-DECLARE_BITOPS_SET(w, uint16_t)
-DECLARE_BITOPS_GET(l, uint32_t)
-DECLARE_BITOPS_SET(l, uint32_t)
+/* Define bitops functions */
+DEFINE_BITOPS_GET(b, uint8_t)
+DEFINE_BITOPS_SET(b, uint8_t)
+DEFINE_BITOPS_GET(w, uint16_t)
+DEFINE_BITOPS_SET(w, uint16_t)
+DEFINE_BITOPS_GET(l, uint32_t)
+DEFINE_BITOPS_SET(l, uint32_t)
 
-int bitops_reverse(int i, int length);
-int bitops_ffs(int i);
-bool bitops_parity(int i);
+static inline int bitops_reverse(int i, int length)
+{
+	uint8_t index;
+	int r = 0;
+	int b;
+
+	/* Reverse bits */
+	for (index = 0; index < length; index++) {
+		b = (i >> (length - index - 1)) & 0x01;
+		r |= b << index;
+	}
+
+	return r;
+}
+
+static inline int bitops_ffs(int i)
+{
+	int bit;
+
+	/* Handle case where no bit is set */
+	if (i == 0)
+		return 0;
+
+	/* Find position of first bit set within argument */
+	for (bit = 1; (i & 0x01) == 0; bit++)
+		i >>= 1;
+
+	return bit;
+}
+
+static inline bool bitops_parity(int i)
+{
+	bool parity = false;
+
+	/* Compute input parity */
+	while (i) {
+		parity = !parity;
+		i = i & (i - 1);
+	}
+
+	return parity;
+}
+
 
 #endif
 
