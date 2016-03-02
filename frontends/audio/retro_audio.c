d3a5efb1e436d6e94520d512ae2a4524dc96a84c
Sebastien Ronsse <sronsse@gmail.com>
2016-03-01 23:24:24 -0800

    frontends: updated retro audio frontend due to resampling redesign.

--- frontends/audio/retro_audio.c
+++ frontends/audio/retro_audio.c
@@ -4,20 +4,17 @@
 #include <util.h>
 
 struct retro_data {
-	float freq;
-	enum audio_format format;
-	int channels;
+	int sampling_rate;
 	retro_audio_sample_t audio_cb;
 	bool enabled;
 };
 
 void retro_audio_fill_timing(struct retro_system_timing *timing);
 
-static bool ret_init(struct audio_frontend *fe, struct audio_specs *specs);
-static void ret_enqueue(struct audio_frontend *fe, void *buffer, int count);
+static bool ret_init(struct audio_frontend *fe, int sampling_rate);
+static void ret_enqueue(struct audio_frontend *fe, int16_t left, int16_t right);
 static void ret_start(struct audio_frontend *fe);
 static void ret_stop(struct audio_frontend *fe);
-static void get_value(void **buffer, int16_t *v);
 
 static struct retro_data retro_data;
 
@@ -34,59 +31,19 @@ void retro_set_audio_sample_batch(retro_audio_sample_batch_t UNUSED(cb))
 void retro_audio_fill_timing(struct retro_system_timing *timing)
 {
 	/* Fill sample rate */
-	timing->sample_rate = retro_data.freq;
+	timing->sample_rate = retro_data.sampling_rate;
 }
 
-void get_value(void **buffer, int16_t *v)
-{
-	uint8_t **buf_u8 = (uint8_t **)buffer;
-	int8_t **buf_s8 = (int8_t **)buffer;
-	uint16_t **buf_u16 = (uint16_t **)buffer;
-	int16_t **buf_s16 = (int16_t **)buffer;
-
-	/* Get value based on format */
-	switch (retro_data.format) {
-	case AUDIO_FORMAT_U8:
-		*v = (*(*buf_u8)++ << 8) - (USHRT_MAX / 2) - 1;
-		break;
-	case AUDIO_FORMAT_S8:
-		*v = *(*buf_s8)++ << 8;
-		break;
-	case AUDIO_FORMAT_U16:
-		*v = *(*buf_u16)++ - (USHRT_MAX / 2) - 1;
-		break;
-	case AUDIO_FORMAT_S16:
-		*v = *(*buf_s16)++;
-		break;
-	}
-}
-
-bool ret_init(struct audio_frontend *UNUSED(fe), struct audio_specs *specs)
+bool ret_init(struct audio_frontend *UNUSED(fe), int sampling_rate)
 {
 	/* Set audio properties */
-	retro_data.freq = specs->freq;
-	retro_data.format = specs->format;
-	retro_data.channels = specs->channels;
+	retro_data.sampling_rate = sampling_rate;
 	retro_data.enabled = false;
 	return true;
 }
 
-void ret_enqueue(struct audio_frontend *UNUSED(fe), void *buffer, int count)
+void ret_enqueue(struct audio_frontend *UNUSED(fe), int16_t left, int16_t right)
 {
-	int16_t left = 0;
-	int16_t right = 0;
-
-	/* Get buffer data only when audio is enabled */
-	if (retro_data.enabled) {
-		/* Get left channel */
-		get_value(&buffer, &left);
-
-		/* Get right channel */
-		right = left;
-		if (retro_data.channels == 2)
-			get_value(&buffer, &right);
-	}
-
 	/* Push sample */
 	retro_data.audio_cb(left, right);
 }
