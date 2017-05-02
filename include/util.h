a6375f9e78cbf4af9bcddc095c8ce8d741b44b92
Sebastien Ronsse <sronsse@gmail.com>
2017-05-02 11:57:03 -0700

    main: added INITIALIZER utility macro to replace constructors/destructors.

--- include/util.h
+++ include/util.h
@@ -4,5 +4,28 @@
 #define ARRAY_SIZE(array) (sizeof(array) / sizeof(array[0]))
 #define UNUSED(x) UNUSED_ ## x __attribute__((__unused__))
 
+#ifdef __cplusplus
+#define INITIALIZER(f) \
+        static void f(void); \
+        struct f##_t_ { f##_t_(void) { f(); } }; static f##_t_ f##_; \
+        static void f(void)
+#elif defined(_MSC_VER)
+#pragma section(".CRT$XCU",read)
+#define INITIALIZER2_(f,p) \
+        static void f(void); \
+        __declspec(allocate(".CRT$XCU")) void (*f##_)(void) = f; \
+        __pragma(comment(linker,"/include:" p #f "_")) \
+        static void f(void)
+#ifdef _WIN64
+#define INITIALIZER(f) INITIALIZER2_(f,"")
+#else
+#define INITIALIZER(f) INITIALIZER2_(f,"_")
+#endif
+#else
+#define INITIALIZER(f) \
+        static void f(void) __attribute__((constructor)); \
+        static void f(void)
+#endif
+
 #endif
 
